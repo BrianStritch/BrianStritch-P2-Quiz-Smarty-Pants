51a6cc9c67acb68441e0e265cf53c689d380d36d
BrianStritch <brians12ca18@gmail.com>
2021-10-17 15:20:35 +0000

    ammend html issues for validateion and JS issues for validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,18 @@
 /* outer entry zone */
 let outerZone = document.getElementById('outer-zone');
-
 let startBtn = document.getElementById('enter');
 startBtn.addEventListener('click', enterSelection);
-
 let rulesBtn = document.getElementById('rules-btn');
 rulesBtn.addEventListener('click', rules);
-
 let rulesList = document.getElementById('rules-screen');
 
 /* selector screen */
-let selectionScreen = document.getElementById('selection-screen');
-    
+let selectionScreen = document.getElementById('selection-screen');    
 let peppaGameOuter = document.getElementById('peppa-game-outer');
-
 let loveGameOuter = document.getElementById('love-game-outer');
 let musicGameOuter = document.getElementById('music-game-outer');
 
-/* functions for outer entry screen and rules list */
+/* functions for outer entry screen and rules modal */
 function enterSelection(){
     outerZone.classList.add('hide');
     selectionScreen.classList.remove('hide');
@@ -28,26 +23,16 @@ function rules(){
 function exitRules(){
     rulesList.classList.add('hide');
 }
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+let modal = document.getElementById("myModal");
+let modalBtn = document.getElementById("modal-Btn");
+let span = document.getElementsByClassName("close")[0];
+modalBtn.onclick = function() {
   modal.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -66,7 +51,7 @@ let endResult;
 let oldScore;
 let oldScores;
 let gameTypeSelectionForIfStatements;
-
+let message;
 
 
 /* ********** peppa pig game variables ****************/
@@ -197,17 +182,15 @@ function startNewGame(gameTypeSelected){
 
 function setNextQuestion(){   
     resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-    console.log(message);
+    showQuestion(shuffledQuestions[currentQuestionIndex]);    
 }
-let message;
+
 function showQuestion(question){
     if (gameTypeSelectionForIfStatements === 'peppa'){                    
         document.getElementById('pep-questions').innerText = question.question;        
     } else if(gameTypeSelectionForIfStatements === 'love'){            
         document.getElementById('love-question').innerText = question.question;      
-    } else if(gameTypeSelectionForIfStatements === 'music'){ 
-        console.log('got inside showquestion music if');           
+    } else if(gameTypeSelectionForIfStatements === 'music'){          
         document.getElementById('music-questions').innerText = question.question;  
     }  
     message = question.message;    
@@ -223,17 +206,14 @@ function showQuestion(question){
         if (gameTypeSelectionForIfStatements === 'peppa'){            
             button.addEventListener('click', selectAnswer);
             answerButtonsElement.appendChild(button); 
-        } else if(gameTypeSelectionForIfStatements === 'love'){ 
-            console.log('this is the test where it appends button');            
+        } else if(gameTypeSelectionForIfStatements === 'love'){          
             button.addEventListener('click', selectAnswer);
             answerButtonsLove.appendChild(button);        
-        } else if(gameTypeSelectionForIfStatements === 'music'){ 
-            console.log('this is the test where it appends button');           
+        } else if(gameTypeSelectionForIfStatements === 'music'){           
             button.addEventListener('click', selectAnswer);
             answerButtonsMusic.appendChild(button);  
         }
-    });
-    console.log('this is the end of the showquestion function');  
+    }); 
 }
 /**
  * function to execute when next button is pressed in game, which calls on the functions of show answerButtons to remove hide
@@ -286,7 +266,6 @@ function selectAnswer(e){
     let buttons = document.getElementsByClassName('answer-btn');
         for (let button of buttons){
                 button.removeEventListener('click', selectAnswer);
-                console.log('inside disable');
             }
     setStatusClass(selectedButton, correct);
     if (gameTypeSelectionForIfStatements === 'peppa'){
@@ -316,7 +295,6 @@ function selectAnswer(e){
         usedQuestions.push('1');
 
     } else{
-        console.log('inside select answer 2.2'); 
         if (gameTypeSelectionForIfStatements === 'peppa'){
             peppaGameEnd.classList.remove('hide');
             peppaGameOuter.classList.add('hide');    
